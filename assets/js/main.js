9f7dfdabf24c8037d32f3b394a96c0cb4a3c68d8
ultimatexilliam <joakim.siiria@gmail.com>
2022-02-11 14:56:10 +0200

    And MOREEE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
-const obj = JSON.parse("https://api.minetools.eu/query/megamaa.com/25565")
-console.log(obj.players.now)
+/* Minecraft server players */
+$(document).ready(()=>{
+    const ip = $(".sip").attr("data-ip");
+    const port = $(".sip").attr("data-port");
+  
+    $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+      if (result.online) {
+        $(".sip").html(result.players.now);
+      } else {
+        $(".playercount").html("Server isn't online!");
+      }
+    });
+  
+    setInterval(()=>{
+      $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+        if (result.online) {
+          $(".sip").html(result.players.now);
+        } else {
+          $(".playercount").html("Server isn't online!");
+        }
+      });
+    }, 500);
+  });
+  
